b33c0a562556b8626830e9ed3db6a0d5568d490e
Orlando Valdez <orlandovald@gmail.com>
2020-06-27 19:23:49 -0400

    Change permissions on prerequisites script

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 # Global variables
 ROOT_DIR="/home/pi/arcade1up"
 GITHUB_REPO="https://github.com/orlandovald/arcade1up.git"
 
-echo "Installing prerequisites"
+echo "*** Installing prerequisites ***"
+echo ""
 
 sudo apt-get update
 sudo apt-get -y install git python-pip
@@ -14,4 +17,5 @@ mkdir -p "${ROOT_DIR}"
 # Clone repo
 git clone "${GITHUB_REPO}" "${ROOT_DIR}"
 
-echo "prerequisites have been installed"
+echo ""
+echo "*** Prerequisites have been installed ***"
