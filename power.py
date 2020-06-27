2610ba8f121d3e8772eae9cf9755dc1684697784
Orlando Valdez <orlandovald@gmail.com>
2020-06-27 18:55:55 -0400

    Add power button script

---
+++ power.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import subprocess
+
+THE_PIN = 5
+ON_STATE = 0
+OFF_STATE = 1
+
+print("Starting " + __file__)
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(THE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+previousState = GPIO.input(THE_PIN)
+
+loop = True
+while loop:
+    GPIO.wait_for_edge(THE_PIN, GPIO.BOTH)
+    currentState = GPIO.input(THE_PIN)
+    if previousState != currentState and currentState == OFF_STATE:
+        subprocess.call("shutdown -h now", shell=True,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        loop = False
+    else:
+        previousState = currentState
+
+GPIO.cleanup(THE_PIN)
+
+print("Finishing " + __file__)
